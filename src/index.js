2354e683d51686e23b30d759efde0d38bea8f3a9
100opps <c0ntact100opps@gmail.com>
2024-02-21 02:42:33 -0300

    ✨ feat: `data_types` validators

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {
+  isString,
+  isNumber,
+  isBoolean,
+  isFunction,
+  isArray
+} from './validators/data_types.js'
+
+export { isString, isNumber, isBoolean, isFunction, isArray }
---
+++ src/validators/data_types.js
@@ -0,0 +1,5 @@
+export const isString = value => typeof value === 'string'
+export const isNumber = value => typeof value === 'number'
+export const isBoolean = value => typeof value === 'boolean'
+export const isFunction = value => typeof value === 'function'
+export const isArray = value => Array.isArray(value)
