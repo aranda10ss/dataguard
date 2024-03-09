ca1dba0b9b8ea488740f774fb947e41ae0b3ff40
aranda10ss <c0ntact100opps@gmail.com>
2024-03-08 22:44:05 -0300

    🐛 fix: module errors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,4 @@
-import {
-  isString,
-  isBoolean,
-  isFunction,
-  isUndefined,
-  isNull,
-  isArray
-} from './validators/data_types.js'
-import {
-  isNumber,
-  isU8,
-  isU16,
-  isU32,
-  isI8,
-  isI16,
-  isI32
-} from './validators/numbers.js'
-import { isEmail } from './validators/emails.js'
+export * from './validators/data_types.js';
+export * from './validators/emails.js';
+export * from './validators/numbers.js';
 
-export {
-  isString,
-  isNumber,
-  isBoolean,
-  isFunction,
-  isUndefined,
-  isNull,
-  isArray,
-  isU8,
-  isU16,
-  isU32,
-  isI8,
-  isI16,
-  isI32,
-  isEmail
-}
